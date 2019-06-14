b47693793680c5deeaa7345135fc2d953bf4dbc4
Michael Wagner <mwagn4@gmail.com>
2019-06-14 12:28:26 +0200

    Adds spec for Order.orderOf

--- order.spec.js
+++ order.spec.js
@@ -144,5 +144,53 @@ describe('Order', () => {
       });
     });
   });
+
+  describe('#orderOf()', () => {
+    describe('with heart/sergeant/officer trump', () => {
+      let order = new Order();
+      order.promote([Suit.heart], [Rank.sergeant, Rank.officer]);
+
+      it('should return coherent order for each card', () => {
+        let cards = [
+          Card[Suit.bell][Rank.seven],
+          Card[Suit.bell][Rank.eight],
+          Card[Suit.bell][Rank.nine],
+          Card[Suit.bell][Rank.king],
+          Card[Suit.bell][Rank.ten],
+          Card[Suit.bell][Rank.ace],
+          Card[Suit.leaf][Rank.seven],
+          Card[Suit.leaf][Rank.eight],
+          Card[Suit.leaf][Rank.nine],
+          Card[Suit.leaf][Rank.king],
+          Card[Suit.leaf][Rank.ten],
+          Card[Suit.leaf][Rank.ace],
+          Card[Suit.acorn][Rank.seven],
+          Card[Suit.acorn][Rank.eight],
+          Card[Suit.acorn][Rank.nine],
+          Card[Suit.acorn][Rank.king],
+          Card[Suit.acorn][Rank.ten],
+          Card[Suit.acorn][Rank.ace],
+          Card[Suit.heart][Rank.seven],
+          Card[Suit.heart][Rank.eight],
+          Card[Suit.heart][Rank.nine],
+          Card[Suit.heart][Rank.king],
+          Card[Suit.heart][Rank.ten],
+          Card[Suit.heart][Rank.ace],
+          Card[Suit.bell][Rank.sergeant],
+          Card[Suit.heart][Rank.sergeant],
+          Card[Suit.leaf][Rank.sergeant],
+          Card[Suit.acorn][Rank.sergeant],
+          Card[Suit.bell][Rank.officer],
+          Card[Suit.heart][Rank.officer],
+          Card[Suit.leaf][Rank.officer],
+          Card[Suit.acorn][Rank.officer]
+        ];
+        for (let card of cards) {
+          let index = cards.indexOf(card);
+          Assert.equal(order.orderOf(card), ++index);
+        }
+      });
+    });
+  });
 });
 

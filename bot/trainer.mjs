aaa61bebe2535aed24438edc10fdf4197b9cb567
Michael Wagner <mwagn4@gmail.com>
2019-06-17 20:01:38 +0200

    Adds custom random function with seed

--- bot/trainer.mjs
+++ bot/trainer.mjs
@@ -4,32 +4,50 @@ import { Game } from '../game.mjs';
 import { Bot } from './bot.mjs';
 import { Brainless } from './brainless.mjs';
 
-export async function train(brain, iterations, callback) {
-  let game = new Game();
+export async function train(brain, samples, iterations) {
+  let bot = new Bot(1, brain);
   let step = 1/4;
 
-  game.onjoin = (index) => {
-    let brain = new Brainless();
-    let bot = new Bot(index, brain);
-    bot.thinktime = 0;
+  do {
+    console.log(`create ${samples} samples with seed`);
+    let rand = mulberry(12345);
+    let count = samples;
+
+    let game = new Game();
+    game.rand = rand;
 
     bot.attach(game);
 
-    return bot;
-  };
+    game.onjoin = (index) => {
+      let brain = new Brainless(rand);
+      let bot = new Bot(index, brain);
+      bot.thinktime = 0;
 
-  game.onproceed = () => {
-    iterations -= step;
-    callback(iterations);
-    return iterations > 0;
-  };
+      bot.attach(game);
 
-  let bot = new Bot(1, brain);
-  bot.attach(game);
+      return bot;
+    };
 
-  callback(iterations);
-  await game.run();
+    game.onproceed = () => {
+      count -= step;
+      return count > 0;
+    };
+
+    await game.run();
+    console.log(`repeating stuff, another ${iterations} left`);
+  } while (--iterations > 0);
+
+  brain.trained = true;
 
   return brain;
+}
+
+function mulberry(seed) {
+  return () => {
+    let value = seed += 0x6D2B79F5;
+    value = Math.imul(value ^ value >>> 15, value | 1);
+    value ^= value + Math.imul(value ^ value >>> 7, value | 61);
+    return ((value ^ value >>> 14) >>> 0) / 4294967296;
+  }
 }
 

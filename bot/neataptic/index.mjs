048f2a50cb74ed867a1fd930d31bc2a0c21c59ab
Michael Wagner <mwagn4@gmail.com>
2019-07-15 19:54:05 +0200

    Adds option to load previous network for training

--- bot/neataptic/index.mjs
+++ bot/neataptic/index.mjs
@@ -5,15 +5,14 @@ import Process from 'process';
 import { Trainer } from '../trainer.mjs';
 import { Brain } from './brain.mjs';
 
-let brain = new Brain();
-
 const options = {
-  callback
+  // Function called after each new generation
+  callback,
 
-  /* additional settings:
-   *   runs: number of iterations for each generation,
-   *   target: number of generations to evolve
-   */
+  // Number of iterations for each generation
+  runs: 1000,
+  // Number of generations to evolve
+  target: 500
 };
 
 Process.stderr.write('Starting training');
@@ -22,7 +21,16 @@ function callback() {
   Process.stderr.write('.');
 };
 
-Trainer.train(brain, options).then((brain) => {
+import('../../brains/network.json').then((network) => {
+  let brain = new Brain();
+  brain.deserialize(network.default);
+
+  return brain;
+}, () => {
+  return new Brain();
+}).then((brain) => {
+  return Trainer.train(brain, options)
+}).then((brain) => {
   Process.stderr.write(` Finished!${OS.EOL}`);
 
   let network = brain.serialize();

1a7ace3a054d59dc72597976cf763d8d225cb937
Michael Wagner <mwagn4@gmail.com>
2019-10-08 14:04:07 +0200

    Logs some stats every n steps

--- bot/deepq/index.mjs
+++ bot/deepq/index.mjs
@@ -1,11 +1,13 @@
 
+import OS from 'os';
+import Process from 'process';
 import File from 'fs';
 import { Trainer } from './trainer.mjs';
 import { GreedyStrategy, ReplayMemory } from './deepq.mjs';
 
 const options = {
   // Function called after each simulation
-  callback: every(10e3, save()),
+  callback: every(10e3, stats(), save()),
 
   // Number of games to simulate
   episodes: 10e6,
@@ -16,12 +18,26 @@ const options = {
 
 Trainer.train(options);
 
+function stats() {
+  let last = Date.now();
+  return ({ steps, count }) => {
+    let now = Date.now();
+    let span = now - last;
+
+    Process.stdout.write(''
+      + `Currently at ${steps} steps, last ${count} steps took ${span} ms`
+      + OS.EOL);
+
+    last = now;
+  };
+}
+
 function save() {
   let version = 1;
-  return ([brain]) => {
+  return ({ brains }) => {
     let name = `Snapshot Gen #${version}`;
 
-    let network = brain.serialize();
+    let network = brains[0].serialize();
     network.name = name;
 
     let json = JSON.stringify(network);
@@ -33,11 +49,14 @@ function save() {
   };
 }
 
-function every(count, fn) {
+function every(count, ...callbacks) {
   let steps = 1;
-  return (...args) => {
+  return (brains) => {
     if (steps++ % count == 0) {
-      fn(...args);
+      let args = { steps, count, brains };
+      for (let callback of callbacks) {
+        callback(args);
+      }
     }
   };
 }
